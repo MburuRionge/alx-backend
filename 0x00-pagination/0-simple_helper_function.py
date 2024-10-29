b979f4a4f124af44d9d926e316fe52fb2c081ea5
MburuRionge <victormburu3@gmail.com>
2024-10-29 02:43:13 +0000

    using page and page_size to paginate

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """the function  two integer arguments page and page_size."""
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int):
     """return a tuple of size two containing a start index and an end index"""
     start = (page - 1) * page_size
     end = start + page_size
