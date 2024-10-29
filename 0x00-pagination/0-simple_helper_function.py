0fd514de7a4a2a29d6718b3e769f4db4f8c2b6b0
MburuRionge <victormburu3@gmail.com>
2024-10-29 02:55:05 +0000

    How to paginate a dataset with hypermedia metadata

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """the function  two integer arguments page and page_size."""
 
-def index_range(page: int, page_size: int):
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """return a tuple of size two containing a start index and an end index"""
     start = (page - 1) * page_size
     end = start + page_size
