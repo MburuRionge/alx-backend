aca417d310b43e61f0fffc729aaeff923fde9097
MburuRionge <victormburu3@gmail.com>
2024-11-08 23:43:10 +0000

    project related to internationalization (i18n) in software development.

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python3
+"""A simple flask app
+"""
 
-"""instantiate the Babel object in my app"""
 
-from flask import Flask render_template
+from flask import Flask, render_template
 from flask_babel import Babel
 
 
+class Config(object):
+    """_summary_
+
+    Returns:
+            _type_: _description_
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# configure the flask app
 app = Flask(__name__)
-
-class Config:
-    """configutation class"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-#apply Config to Flask app
 app.config.from_object(Config)
-
-#instantiate Babel and store it in a variable
 babel = Babel(app)
+
 
 @app.route('/')
 def index():
-    return render_template("1-index.html")
+    """_summary_
+    """
+    return render_template('1-index.html')
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
